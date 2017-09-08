c0bd8f14a65dfd4152452232f5eae191841930b8
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2017-09-08 14:24:48 -0300

    part 5

--- snake.cpp
+++ snake.cpp
@@ -42,13 +42,13 @@ snakeclass::snakeclass()
     srand(time(0));
     putfood();
 
-    for(int i = 0; i < maxwidth - 1; i++)
+    for (int i = 0; i < maxwidth - 1; i++)
     {
         move(maxheight - 2, i);
         addch(oldalchar);
     }
 
-    for(int i = 0; i < maxheight - 1; i++)
+    for (int i = 0; i < maxheight - 1; i++)
     {
         move(i, 0);
         addch(oldalchar);
@@ -78,6 +78,136 @@ snakeclass::~snakeclass()
 
 void snakeclass::putfood()
 {
-    food.x = 10;
-    food.y = 10;
+    while (1)
+    {
+        int tmpx = rand() % maxwidth + 1;
+        int tmpy = rand() % maxheight + 1;
+
+        for (int i = 0; i < snake.size(); i++)
+        {
+            if (snake[i].x == tmpx && snake[i].y == tmpy)
+            {
+                continue;
+            }
+        }
+
+        if (tmpx >= maxwidth - 2 || tmpy >= maxheight - 3)
+        {
+            continue;
+        }
+
+        food.x = tmpx;
+        food.y = tmpy;
+
+        break;
+    }
+
+    move(food.y, food.x);
+    addch(foo);
+    refresh();
 }
+
+bool snakeclass::collision()
+{
+    if (snake[0].x == 0 || snake[0].x == maxwidth - 1 || snake[0].y == 0 || snake[0].y == maxheight - 2 )
+    {
+        return true;
+    }
+
+    for (int i = 2; i < snake.size(); i++)
+    {
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y)
+        {
+            return true;
+        }
+    }
+
+    if (snake[0].x == food.x && snake[0].y == food.y)
+    {
+        get = true;
+        putfood();
+        points += 10;
+        move(maxheight - 1, 0);
+        printw("%d", points);
+
+        if (points % 100 == 0)
+        {
+            del -= 10000;
+        }
+    }
+    else
+    {
+        get = false;
+    }
+
+    return false;
+}
+
+void snakeclass::movesnake()
+{
+    int temp = getch();
+    switch(temp)
+    {
+        case KEY_LEFT:
+            if (direction != 'r')
+            {
+                direction = 'l';
+            }
+            break;
+
+        case KEY_UP:
+            if (direction != 'd')
+            {
+                direction = 'u';
+            }
+            break;
+
+        case KEY_RIGHT:
+            if (direction != 'l')
+            {
+                direction = 'r';
+            }
+            break;
+
+        case KEY_DOWN:
+            if (direction != 'u')
+            {
+                direction = 'd';
+            }
+            break;
+
+        case KEY_BACKSPACE:
+            direction = 'q';
+            break;
+    }
+
+    if (!get)
+    {
+        move(snake[snake.size() - 1].y, snake[snake.size() - 1].x);
+        addch(' ');
+        refresh();
+        snake.pop_back();
+    }
+
+    if (direction == 'l')
+    {
+        snake.insert(snake.begin(), snakepart(snake[0].x - 1, snake[1].y));
+    }
+    else if (direction == 'r')
+    {
+        snake.insert(snake.begin(), snakepart(snake[0].x + 1, snake[1].y));
+    }
+    else if (direction == 'u')
+    {
+        snake.insert(snake.begin(), snakepart(snake[0].x, snake[1].y - 1));
+    }
+    else if (direction == 'd')
+    {
+        snake.insert(snake.begin(), snakepart(snake[0].x, snake[1].y + 1));
+    }
+
+    move(snake[0].y, snake[0].x);
+    addch(partchar);
+    refresh();
+}
+
